73a1323bfc5618a004c0429dd198f134fa6d29b6
sallyh0812 <sallyhuang0812@gmail.com>
2023-12-12 00:02:07 +0800

    Finish Assignment6 my first clear bricks game!

--- Assignment6/breakout.py
+++ Assignment6/breakout.py
@@ -10,21 +10,27 @@ Name: Sally 111613025
 from campy.gui.events.timer import pause
 from breakoutgraphics import BreakoutGraphics
 
-FRAME_RATE = 10         # 100 frames per second
-NUM_LIVES = 3			# Number of attempts
+FRAME_RATE = 10  # 100 frames per second
+NUM_LIVES = 3  # Number of attempts
 
 
 def main():
-    graphics = BreakoutGraphics()
+    game = BreakoutGraphics()
     life = NUM_LIVES
-
-    # Add the animation loop here!
-    while life and graphics.brick_amount:
-        graphics.ball.move(graphics.get_ball_speed_x(), graphics.get_ball_speed_y())
-        if graphics.check_collision() == "drop out":
+    
+    # the animation loop, stop when life == 0 or brick_amount == 0
+    while life and game.get_brick_amount():
+        game.ball.move(game.get_ball_speed_x(), game.get_ball_speed_y())
+        
+        # check and change speed for all the collision between the ball, the bricks, the paddle, and the wall
+        game.check_collision()
+        
+        if game.check_drop_out():  # if the ball drops out the window
             life -= 1
         pause(FRAME_RATE)
-    graphics.reset_ball()
+    
+    # reset ball after the end of the game
+    game.reset_ball()
 
 
 if __name__ == '__main__':
--- Assignment6/breakoutgraphics.py
+++ Assignment6/breakoutgraphics.py
@@ -14,8 +14,8 @@ import random
 BRICK_SPACING = 5  # Space between bricks (in pixels). This space is used for horizontal and vertical spacing
 BRICK_WIDTH = 40  # Width of a brick (in pixels)
 BRICK_HEIGHT = 15  # Height of a brick (in pixels)
-BRICK_ROWS = 7  # Number of rows of bricks
-BRICK_COLS = 5  # Number of columns of bricks
+BRICK_ROWS = 10  # Number of rows of bricks
+BRICK_COLS = 10  # Number of columns of bricks
 BRICK_OFFSET = 50  # Vertical offset of the topmost brick from the window top (in pixels)
 BALL_RADIUS = 10  # Radius of the ball (in pixels)
 PADDLE_WIDTH = 75  # Width of the paddle (in pixels)
@@ -26,10 +26,13 @@ MAX_X_SPEED = 5  # Maximum initial horizontal speed for the ball
 
 
 class BreakoutGraphics:
-    
     def __init__(self, ball_radius=BALL_RADIUS, paddle_width=PADDLE_WIDTH, paddle_height=PADDLE_HEIGHT,
                  paddle_offset=PADDLE_OFFSET, brick_rows=BRICK_ROWS, brick_cols=BRICK_COLS, brick_width=BRICK_WIDTH,
                  brick_height=BRICK_HEIGHT, brick_offset=BRICK_OFFSET, brick_spacing=BRICK_SPACING, title='Breakout'):
+        """
+        Make the initial window with some bricks, a ball, a paddle, and start the mouse listener
+        """
+        
         # Create a graphical window, with some extra space
         window_width = brick_cols * (brick_width + brick_spacing) - brick_spacing
         window_height = brick_offset + 3 * (brick_rows * (brick_height + brick_spacing) - brick_spacing)
@@ -47,8 +50,8 @@ class BreakoutGraphics:
                         y=(self.window.height - self.ball.height) / 2)
         
         # Default initial velocity for the ball
-        self.ball.__dx = 0
-        self.ball.__dy = 0
+        self.__ball_dx = 0
+        self.__ball_dy = 0
         
         # Draw bricks
         current_y = brick_offset
@@ -63,16 +66,20 @@ class BreakoutGraphics:
                 current_x += self.brick.width + brick_spacing
             current_y += self.brick.height + brick_spacing
             if i % 2 == 1:
+                #  change bricks' color every 2 rows
                 self.change_brick_color()
+        
+        # Default initial bricks amount
+        self.__brick_amount = brick_cols * brick_rows
         
         # Initialize our mouse listeners
         onmouseclicked(self.set_ball_speed)
         onmousemoved(self.move_paddle)
-        
-        ##
-        self.brick_amount = brick_cols * brick_rows
     
     def change_brick_color(self):
+        """
+        Change the bricks' color according to the current brick color when adding bricks to the window
+        """
         if self.brick_color == 'red':
             self.brick_color = 'orange'
         elif self.brick_color == 'orange':
@@ -85,66 +92,101 @@ class BreakoutGraphics:
             self.brick_color = 'red'
     
     def move_paddle(self, mouse):
-        if mouse.x + self.paddle.width / 2 > self.window.width:
+        """
+        Set paddle.x to current mouse.x
+        """
+        if mouse.x + self.paddle.width / 2 > self.window.width:  # right boundary
             self.paddle.x = self.window.width - self.paddle.width
-        elif mouse.x - self.paddle.width / 2 < 0:
+        elif mouse.x - self.paddle.width / 2 < 0:  # left boundary
             self.paddle.x = 0
         else:
             self.paddle.x = mouse.x - self.paddle.width / 2
     
     def set_ball_speed(self, event):
-        if self.ball.__dx == 0 and self.ball.__dy == 0:
-            self.ball.__dx = random.randint(1, MAX_X_SPEED)
-            self.ball.__dy = INITIAL_Y_SPEED
+        """
+        Give a random speed to ball when clicked
+        """
+        if self.__ball_dx == 0 and self.__ball_dy == 0:  # do nothing if ball is moving
+            self.__ball_dx = random.randint(1, MAX_X_SPEED)
+            self.__ball_dy = INITIAL_Y_SPEED
             if random.random() > 0.5:
-                self.ball.__dx = -self.ball.__dx
-    
-    def get_ball_speed_x(self):
-        return self.ball.__dx
-    
-    def get_ball_speed_y(self):
-        return self.ball.__dy
+                self.__ball_dx = -self.__ball_dx
     
     def check_collision(self):
-        x = self.ball.x
-        y = self.ball.y
-        r = self.ball.width / 2
-        check1 = self.window.get_object_at(x, y)
-        check2 = self.window.get_object_at(x + r * 2, y)
-        check3 = self.window.get_object_at(x, y + r * 2)
-        check4 = self.window.get_object_at(x + r * 2, y + r * 2)
+        """
+        Check if the ball hit the paddle, the bricks, the walls.
+        Hit the paddle: bounce up
+        Hit the bricks: remove the brick and bounce
+        Hit the walls: bounce
+        Bounce by changing the direction of speed (__ball_dx or __ball_dy)
+        
+        """
+        ball_x = self.ball.x
+        ball_y = self.ball.y
+        ball_r = self.ball.width / 2
+        check1 = self.window.get_object_at(ball_x, ball_y)  # upper left
+        check2 = self.window.get_object_at(ball_x + ball_r * 2, ball_y)  # upper right
+        check3 = self.window.get_object_at(ball_x, ball_y + ball_r * 2)  # lower left
+        check4 = self.window.get_object_at(ball_x + ball_r * 2, ball_y + ball_r * 2)  # lower right
         if check1 is not None:
-            if check1 is not self.paddle:
+            if check1 is not self.paddle:  # if hit the brick
                 self.window.remove(check1)
-                self.brick_amount -= 1
-            self.ball.__dy *= -1
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+            else:  # if hit the paddle
+                # make sure it goes up even when it hits the paddle from the side
+                self.__ball_dy = -abs(self.__ball_dy)
         elif check2 is not None:
             if check2 is not self.paddle:
                 self.window.remove(check2)
-                self.brick_amount -= 1
-            self.ball.__dy *= -1
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+            else:
+                self.__ball_dy = -abs(self.__ball_dy)
         elif check3 is not None:
             if check3 is not self.paddle:
                 self.window.remove(check3)
-                self.brick_amount -= 1
-            self.ball.__dy *= -1
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+            else:
+                self.__ball_dy = -abs(self.__ball_dy)
         elif check4 is not None:
             if check4 is not self.paddle:
                 self.window.remove(check4)
-                self.brick_amount -= 1
-            self.ball.__dy *= -1
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+            else:
+                self.__ball_dy = -abs(self.__ball_dy)
         
-        if self.ball.y > self.window.height:
-            self.reset_ball()
-            return "drop out"
-        
+        #  check if hit the wall
         if self.ball.x + self.ball.width >= self.window.width or self.ball.x <= 0:
-            self.ball.__dx *= -1
+            self.__ball_dx *= -1
         if self.ball.y <= 0:
-            self.ball.__dy *= -1
+            self.__ball_dy *= -1
+            
+    def check_drop_out(self):
+        """
+        check if dropped out of window
+        :return: "drop out", when the ball drops out of window, for user to decrease the lives
+        """
+        if self.ball.y >= self.window.height:
+            self.reset_ball()
+            return True
     
     def reset_ball(self):
+        """
+        reset ball to the initial position and with 0 speed
+        """
         self.ball.x = (self.window.width - self.ball.width) / 2
         self.ball.y = (self.window.height - self.ball.height) / 2
-        self.ball.__dx = 0
-        self.ball.__dy = 0
+        self.__ball_dx = 0
+        self.__ball_dy = 0
+    
+    def get_ball_speed_x(self):
+        return self.__ball_dx
+    
+    def get_ball_speed_y(self):
+        return self.__ball_dy
+    
+    def get_brick_amount(self):
+        return self.__brick_amount
