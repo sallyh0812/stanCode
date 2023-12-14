fad20d27318cad6e4bb28ab32af4f3d364625073
sallyh0812 <sallyhuang0812@gmail.com>
2023-12-14 14:12:30 +0800

    Extension v1 double_ball, long_paddle, short_paddle, free_paddle

---
+++ Assignment6/breakout_ext.py
@@ -0,0 +1,46 @@
+"""
+stanCode Breakout Project
+Adapted from Eric Roberts's Breakout by
+Sonja Johnson-Yu, Kylie Jue, Nick Bowman,
+and Jerry Liao.
+
+Name: Sally 111613025
+"""
+
+from campy.gui.events.timer import pause
+from breakoutgraphics_ext import BreakoutGraphics_ext
+
+FRAME_RATE = 10  # 100 frames per second
+NUM_LIVES = 3  # Number of attempts
+
+
+def main():
+    game = BreakoutGraphics_ext()
+    life = NUM_LIVES
+    
+    # the animation loop, stop when life == 0 or brick_amount == 0
+    while life and game.get_brick_amount():
+        game.ball.move(game.get_ball_speed_x(), game.get_ball_speed_y())
+        
+        # check and change speed for all the collision between the ball, the bricks, the paddle, and the wall
+        game.check_game_ball1()
+        
+        if game.ball2 is not None:
+            game.ball2.move(game.get_ball2_speed_x(), game.get_ball2_speed_y())
+            game.check_game_ball2()
+            game.check_drop_out2()
+            
+        if game.is_long_paddle:
+            if not game.long_paddle_time:
+                game.reset_paddle_width()
+        
+        if game.check_drop_out1():  # if the ball drops out the window
+            life -= 1
+        pause(FRAME_RATE)
+    
+    # reset ball after the end of the game
+    game.reset_ball()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Assignment6/breakoutgraphics_ext.py
@@ -0,0 +1,491 @@
+"""
+stanCode Breakout Project
+Adapted from Eric Roberts's Breakout by
+Sonja Johnson-Yu, Kylie Jue, Nick Bowman, 
+and Jerry Liao.
+
+Name: Sally 111613025
+"""
+from campy.graphics.gwindow import GWindow
+from campy.graphics.gobjects import GOval, GRect, GLabel
+from campy.graphics.gimage import GImage
+from campy.gui.events.mouse import onmouseclicked, onmousemoved
+import random
+
+BRICK_SPACING = 5  # Space between bricks (in pixels). This space is used for horizontal and vertical spacing
+BRICK_WIDTH = 40  # Width of a brick (in pixels)
+BRICK_HEIGHT = 15  # Height of a brick (in pixels)
+BRICK_ROWS = 10  # Number of rows of bricks
+BRICK_COLS = 10  # Number of columns of bricks
+BRICK_OFFSET = 50  # Vertical offset of the topmost brick from the window top (in pixels)
+BALL_RADIUS = 10  # Radius of the ball (in pixels)
+PADDLE_WIDTH = 75  # Width of the paddle (in pixels)
+PADDLE_HEIGHT = 8  # Height of the paddle (in pixels)
+PADDLE_OFFSET = 50  # Vertical offset of the paddle from the window bottom (in pixels)
+INITIAL_Y_SPEED = 4  # Initial vertical speed for the ball
+MAX_X_SPEED = 5  # Maximum initial horizontal speed for the ball
+SCORE_FONT = "-25"
+SCORE_OFFSET = 8
+STATUS_BOARD_HEIGHT = 50
+LONG_PADDLE_TIME = 5
+IMG_FILE = "mole.png"
+
+
+# class Brick:
+#     def __init__(self, brick_width=BRICK_WIDTH,
+#                  brick_height=BRICK_HEIGHT, brick_life = 4):
+#         self.width = brick_width
+#         self.height = brick_height
+#         self.brick_life = brick_life
+#         self.brick = GRect(brick_width, brick_height)
+#         self.brick.filled = True
+#         if self.brick_life == 4:
+#             self.brick_color = 'red'
+#         elif self.brick_life == 3:
+#             self.brick_color = 'orange'
+#         elif self.brick_life == 2:
+#             self.brick_color = 'yellow'
+#         self.brick.fill_color = self.brick_color
+        
+
+class BreakoutGraphics_ext:
+    def __init__(self, ball_radius=BALL_RADIUS, paddle_width=PADDLE_WIDTH, paddle_height=PADDLE_HEIGHT,
+                 paddle_offset=PADDLE_OFFSET, brick_rows=BRICK_ROWS, brick_cols=BRICK_COLS, brick_width=BRICK_WIDTH,
+                 brick_height=BRICK_HEIGHT, brick_offset=BRICK_OFFSET, brick_spacing=BRICK_SPACING,
+                 status_board_height=STATUS_BOARD_HEIGHT, score_font=SCORE_FONT, score_offset=SCORE_OFFSET,
+                 img_file=IMG_FILE, title='Breakout'):
+        """
+        Make the initial window with some bricks, a ball, a paddle, and start the mouse listener
+        """
+        
+        # Create a graphical window, with some extra space
+        window_width = brick_cols * (brick_width + brick_spacing) - brick_spacing
+        window_height = brick_offset + 3 * (brick_rows * (brick_height + brick_spacing) - brick_spacing) + paddle_offset
+        self.window = GWindow(width=window_width, height=window_height, title=title)
+        # Default score board
+        self.score = 0
+        self.score_board = GLabel(f"Score: {self.score}")
+        self.score_board.font = score_font
+        # Default life board
+        self.life = 3
+        self.life_board = GLabel(f"Lives: {self.life}")
+        self.life_board.font = score_font
+        # img
+        self.img = GImage(img_file)
+        self.window.add(self.img, x=0, y=brick_offset)
+        
+        # status board
+        self.status_board_height = self.score_board.height * 2
+        self.status_board = GRect(window_width, status_board_height, x=0, y=window_height - self.status_board_height)
+        self.status_board.filled = True
+        self.status_board.fill_color = 'blue'
+        self.window.add(self.status_board)
+        self.window.add(self.score_board, x=score_offset, y=self.window.height - score_offset)
+        self.window.add(self.life_board, x=self.window.width - self.life_board.width - score_offset,
+                        y=self.window.height - score_offset)
+        # Create a paddle
+        self.paddle = GRect(paddle_width, paddle_height)
+        self.paddle.filled = True
+        self.window.add(self.paddle, x=(self.window.width - self.paddle.width) / 2,
+                        y=self.window.height - paddle_offset - self.status_board_height)
+        # Center a filled ball in the graphical window
+        self.ball = GOval(ball_radius * 2, ball_radius * 2)
+        self.ball.filled = True
+        self.window.add(self.ball, x=(self.window.width - self.ball.width) / 2,
+                        y=(self.window.height - self.ball.height) / 2)
+        
+        # Default initial velocity for the ball
+        self.__ball_dx = 0
+        self.__ball_dy = 0
+        
+        # Draw bricks
+        current_y = brick_offset
+        self.brick_color = 'gray'
+        for i in range(brick_rows):
+            current_x = 0
+            for j in range(brick_cols):
+                self.brick = GRect(brick_width, brick_height)
+                self.brick.filled = True
+                self.brick.fill_color = self.brick_color
+                # self.brick = Brick(brick_life=4)
+                self.window.add(self.brick, x=current_x, y=current_y)
+                current_x += self.brick.width + brick_spacing
+            current_y += self.brick.height + brick_spacing
+            if i % 2 == 1:
+                #  change bricks' color every 2 rows
+                self.change_brick_color()
+        
+        # Default initial bricks amount
+        self.__brick_amount = brick_cols * brick_rows
+        
+        # shop
+        self.double_ball = GRect(20, 20)
+        self.double_ball.filled = True
+        self.double_ball.fill_color = 'green'
+        self.window.add(self.double_ball, x=0, y=0)
+        
+        self.longer_paddle = GRect(20, 20)
+        self.longer_paddle.filled = True
+        self.longer_paddle.fill_color = 'yellow'
+        self.window.add(self.longer_paddle, x=25, y=0)
+        
+        self.shorten_paddle = GRect(20, 20)
+        self.shorten_paddle.filled = True
+        self.shorten_paddle.fill_color = 'red'
+        self.window.add(self.shorten_paddle, x=50, y=0)
+
+        self.free_paddle = GRect(20, 20)
+        self.free_paddle.filled = True
+        self.free_paddle.fill_color = 'blue'
+        self.window.add(self.free_paddle, x=75, y=0)
+
+        self.double_score = GRect(20, 20)
+        self.double_score.filled = True
+        self.double_score.fill_color = 'purple'
+        self.window.add(self.double_score, x=100, y=0)
+        
+        #
+        self.ball2 = None
+        self.__ball2_dx = 0
+        self.__ball2_dy = 0
+        
+        #
+        self.is_long_paddle = False
+        self.long_paddle_time = LONG_PADDLE_TIME
+        
+        #
+        self.is_free_paddle=False
+        
+        #
+        self.paddle_area_lower = self.paddle.y
+        self.paddle_area_upper = self.ball.y + paddle_offset
+        
+        # Initialize our mouse listeners
+        onmouseclicked(self.set_ball_speed)
+        onmousemoved(self.move_paddle)
+    
+    def change_brick_color(self):
+        """
+        Change the bricks' color according to the current brick color when adding bricks to the window
+        """
+        if self.brick_color == 'red':
+            self.brick_color = 'orange'
+        elif self.brick_color == 'orange':
+            self.brick_color = 'yellow'
+        elif self.brick_color == 'yellow':
+            self.brick_color = 'green'
+        elif self.brick_color == 'green':
+            self.brick_color = 'blue'
+        elif self.brick_color == 'blue':
+            self.brick_color = 'red'
+    
+    def move_paddle(self, mouse):
+        """
+        Set paddle.x to current mouse.x
+        """
+        if self.is_free_paddle:
+            if mouse.y - self.paddle.height / 2 < self.paddle_area_upper:  # right boundary
+                self.paddle.y = self.paddle_area_upper
+            elif mouse.y - self.paddle.height / 2 > self.paddle_area_lower:  # left boundary
+                self.paddle.y = self.paddle_area_lower
+            else:
+                self.paddle.y = mouse.y - self.paddle.height / 2
+    
+        if mouse.x + self.paddle.width / 2 > self.window.width:  # right boundary
+            self.paddle.x = self.window.width - self.paddle.width
+        elif mouse.x - self.paddle.width / 2 < 0:  # left boundary
+            self.paddle.x = 0
+        else:
+            self.paddle.x = mouse.x - self.paddle.width / 2
+    
+    def set_ball_speed(self, event):
+        """
+        Give a random speed to ball when clicked
+        """
+        obj = self.window.get_object_at(event.x, event.y)
+        if obj is self.double_ball:
+            self.add_ball2()
+        elif obj is self.longer_paddle:
+            self.add_paddle_width()
+        elif obj is self.shorten_paddle:
+            self.decrease_paddle_width()
+        elif obj is self.free_paddle:
+            self.freeing_paddle()
+        elif self.__ball_dx == 0 and self.__ball_dy == 0:  # do nothing if ball is moving
+            self.__ball_dx = random.randint(1, MAX_X_SPEED)
+            self.__ball_dy = INITIAL_Y_SPEED
+            if random.random() > 0.5:
+                self.__ball_dx = -self.__ball_dx
+    
+    def add_ball2(self, ball_radius=BALL_RADIUS):
+        self.ball2 = GOval(ball_radius * 2, ball_radius * 2)
+        self.ball2.filled = True
+        self.ball2.fill_color = 'purple'
+        self.window.add(self.ball2, x=(self.window.width - self.ball.width) / 2,
+                        y=(self.window.height - self.ball.height) / 2)
+        self.__ball2_dx = random.randint(1, MAX_X_SPEED)
+        self.__ball2_dy = random.randint(4, INITIAL_Y_SPEED)
+        if random.random() > 0.5:
+            self.__ball2_dx = -self.__ball2_dx
+    
+    def add_paddle_width(self, paddle_width=PADDLE_WIDTH, paddle_height=PADDLE_HEIGHT,
+                         paddle_offset=PADDLE_OFFSET):
+        old_paddle_x = self.paddle.x
+        old_paddle_width = self.paddle.width
+        self.window.remove(self.paddle)
+        self.paddle = GRect(old_paddle_width + 5, paddle_height)
+        self.paddle.filled = True
+        self.window.add(self.paddle, x=old_paddle_x,
+                        y=self.window.height - paddle_offset - self.status_board_height)
+        self.is_long_paddle = True
+        
+    def freeing_paddle(self):
+        self.is_free_paddle = True
+    
+    def decrease_paddle_width(self, paddle_width=PADDLE_WIDTH, paddle_height=PADDLE_HEIGHT,
+                              paddle_offset=PADDLE_OFFSET):
+        old_paddle_x = self.paddle.x
+        old_paddle_width = self.paddle.width
+        self.window.remove(self.paddle)
+        self.paddle = GRect(old_paddle_width - 5, paddle_height)
+        self.paddle.filled = True
+        self.window.add(self.paddle, x=old_paddle_x,
+                        y=self.window.height - paddle_offset - self.status_board_height)
+        self.is_long_paddle = True
+    
+    def reset_paddle_width(self, paddle_width=PADDLE_WIDTH, paddle_height=PADDLE_HEIGHT,
+                           paddle_offset=PADDLE_OFFSET):
+        old_paddle_x = self.paddle.x
+        self.window.remove(self.paddle)
+        self.paddle = GRect(paddle_width, paddle_height)
+        self.paddle.filled = True
+        self.window.add(self.paddle, x=old_paddle_x,
+                        y=self.window.height - paddle_offset - self.status_board_height)
+        self.is_long_paddle = False
+    
+    def check_game_ball1(self):
+        ball_x = self.ball.x
+        ball_y = self.ball.y
+        ball_r = self.ball.width / 2
+        check1 = self.window.get_object_at(ball_x, ball_y)  # upper left
+        check2 = self.window.get_object_at(ball_x + ball_r * 2, ball_y)  # upper right
+        check3 = self.window.get_object_at(ball_x, ball_y + ball_r * 2)  # lower left
+        check4 = self.window.get_object_at(ball_x + ball_r * 2, ball_y + ball_r * 2)  # lower right
+        if check1 is not None:
+            if check1 is self.paddle:
+                if self.is_long_paddle:
+                    self.long_paddle_time -= 1
+                self.__ball_dy = -abs(self.__ball_dy)
+            elif check1 is self.status_board:
+                pass
+            elif check1 is self.score_board:
+                pass
+            elif check1 is self.life_board:
+                pass
+            elif check1 is self.ball2:
+                pass
+            else:
+                self.window.remove(check1)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+        elif check2 is not None:
+            if check2 is self.paddle:
+                if self.is_long_paddle:
+                    self.long_paddle_time -= 1
+                self.__ball_dy = -abs(self.__ball_dy)
+            elif check2 is self.status_board:
+                pass
+            elif check2 is self.score_board:
+                pass
+            elif check2 is self.life_board:
+                pass
+            elif check2 is self.ball2:
+                pass
+            else:
+                self.window.remove(check2)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+        elif check3 is not None:
+            if check3 is self.paddle:
+                if self.is_long_paddle:
+                    self.long_paddle_time -= 1
+                self.__ball_dy = -abs(self.__ball_dy)
+            elif check3 is self.status_board:
+                pass
+            elif check3 is self.score_board:
+                pass
+            elif check3 is self.life_board:
+                pass
+            elif check3 is self.ball2:
+                pass
+            else:
+                self.window.remove(check3)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+        elif check4 is not None:
+            if check4 is self.paddle:
+                if self.is_long_paddle:
+                    self.long_paddle_time -= 1
+                self.__ball_dy = -abs(self.__ball_dy)
+            elif check4 is self.status_board:
+                pass
+            elif check4 is self.score_board:
+                pass
+            elif check4 is self.life_board:
+                pass
+            elif check4 is self.ball2:
+                pass
+            else:
+                self.window.remove(check4)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball_dy *= -1
+        
+        #  check if hit the wall
+        if self.ball.x + self.ball.width >= self.window.width or self.ball.x <= 0:
+            self.__ball_dx *= -1
+        if self.ball.y <= 0:
+            self.__ball_dy *= -1
+    
+    def check_game_ball2(self):
+        """
+        Check if the ball hit the paddle, the bricks, the walls.
+        Hit the paddle: bounce up
+        Hit the bricks: remove the brick and bounce
+        Hit the walls: bounce
+        Bounce by changing the direction of speed (__ball_dx or __ball_dy)
+
+        """
+        ball_x = self.ball2.x
+        ball_y = self.ball2.y
+        ball_r = self.ball2.width / 2
+        check1 = self.window.get_object_at(ball_x, ball_y)  # upper left
+        check2 = self.window.get_object_at(ball_x + ball_r * 2, ball_y)  # upper right
+        check3 = self.window.get_object_at(ball_x, ball_y + ball_r * 2)  # lower left
+        check4 = self.window.get_object_at(ball_x + ball_r * 2, ball_y + ball_r * 2)  # lower right
+        if check1 is not None:
+            if check1 is self.paddle:
+                self.__ball2_dy = -abs(self.__ball2_dy)
+            elif check1 is self.status_board:
+                pass
+            elif check1 is self.score_board:
+                pass
+            elif check1 is self.life_board:
+                pass
+            elif check1 is self.ball:
+                pass
+            else:
+                self.window.remove(check1)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball2_dy *= -1
+        elif check2 is not None:
+            if check2 is self.paddle:
+                self.__ball2_dy = -abs(self.__ball2_dy)
+            elif check2 is self.status_board:
+                pass
+            elif check2 is self.score_board:
+                pass
+            elif check2 is self.life_board:
+                pass
+            elif check2 is self.ball:
+                pass
+            else:
+                self.window.remove(check2)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball2_dy *= -1
+        elif check3 is not None:
+            if check3 is self.paddle:
+                self.__ball2_dy = -abs(self.__ball2_dy)
+            elif check3 is self.status_board:
+                pass
+            elif check3 is self.score_board:
+                pass
+            elif check3 is self.life_board:
+                pass
+            elif check3 is self.ball:
+                pass
+            else:
+                self.window.remove(check3)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball2_dy *= -1
+        elif check4 is not None:
+            if check4 is self.paddle:
+                self.__ball2_dy = -abs(self.__ball2_dy)
+            elif check4 is self.status_board:
+                pass
+            elif check4 is self.score_board:
+                pass
+            elif check4 is self.life_board:
+                pass
+            elif check4 is self.ball:
+                pass
+            else:
+                self.window.remove(check4)
+                self.add_score()
+                self.__brick_amount -= 1
+                self.__ball2_dy *= -1
+        
+        #  check if hit the wall
+        if self.ball2.x + self.ball2.width >= self.window.width or self.ball2.x <= 0:
+            self.__ball2_dx *= -1
+        if self.ball.y <= 0:
+            self.__ball2_dy *= -1
+            
+    def unfree_paddle(self):
+        self.paddle.y = self.paddle_area_lower
+        self.is_free_paddle = False
+    
+    def check_drop_out1(self):
+        """
+        check if dropped out of window
+        :return: "drop out", when the ball drops out of window, for user to decrease the lives
+        """
+        if self.ball.y >= self.window.height - self.status_board.height:
+            self.reset_ball()
+            self.life -= 1
+            self.life_board.text = f"Lives: {self.life}"
+            return True
+    
+    def check_drop_out2(self):
+        """
+        check if dropped out of window
+        :return: "drop out", when the ball drops out of window, for user to decrease the lives
+        """
+        if self.ball2.y >= self.window.height - self.status_board.height:
+            self.window.remove(self.ball2)
+    
+    def reset_ball(self):
+        """
+        reset ball to the initial position and with 0 speed
+        """
+        self.ball.x = (self.window.width - self.ball.width) / 2
+        self.ball.y = (self.window.height - self.ball.height) / 2
+        self.__ball_dx = 0
+        self.__ball_dy = 0
+        self.unfree_paddle()
+        self.reset_paddle_width()
+    
+    def get_ball_speed_x(self):
+        return self.__ball_dx
+    
+    def get_ball_speed_y(self):
+        return self.__ball_dy
+    
+    def get_ball2_speed_x(self):
+        return self.__ball2_dx
+    
+    def get_ball2_speed_y(self):
+        return self.__ball2_dy
+    
+    def get_brick_amount(self):
+        return self.__brick_amount
+    
+    def add_score(self):
+        self.score += 1
+        self.score_board.text = f"Score: {self.score}"
